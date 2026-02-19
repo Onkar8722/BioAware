b7d214f77b5c1659733ac4e9fbad765bce883513
Onkar8722 <onkark1045@gmail.com>
2026-02-19 14:25:46 +0530

    Updation

--- alerts/notifier.py
+++ alerts/notifier.py
@@ -1,8 +1,8 @@
 import requests
 
 # Replace with your actual bot token and chat ID
-TELEGRAM_BOT_TOKEN = "7758824935:AAEf2pust3C6vuqidcrGy67p2buucJ8rPjc"
-CHAT_ID = "1409341093"
+TELEGRAM_BOT_TOKEN = ""
+CHAT_ID = ""
 
 
 def send_telegram_alert(message):
